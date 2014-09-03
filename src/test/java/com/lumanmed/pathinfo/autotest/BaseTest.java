799611f9475f9d768ce5a715c97cf6b9562e3a93
jwang25 <jwang25@ebay.com>
2014-09-03 14:06:00 +0800

    Add user tests.

--- src/test/java/com/lumanmed/pathinfo/autotest/BaseTest.java
+++ src/test/java/com/lumanmed/pathinfo/autotest/BaseTest.java
@@ -3,12 +3,17 @@
  */
 package com.lumanmed.pathinfo.autotest;
 
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 /**
  * @author jwang25
@@ -16,24 +21,50 @@ import org.testng.annotations.BeforeTest;
  */
 public abstract class BaseTest {
 	protected WebDriver driver;
-	
+	protected String pathinfoUrl;
+
+	@Parameters({"pathinfoUrl"})
 	@BeforeTest
-	public void beforeTest() {
+	public void beforeTest(String pathinfoUrl) {
+		this.pathinfoUrl = pathinfoUrl;
 		driver = new FirefoxDriver();
 	}
-	
+
 	@BeforeMethod
 	public void beforeMethod() {
-		
+
 	}
-	
+
 	@AfterMethod
 	public void afterMethod() {
-		
+
 	}
-	
+
 	@AfterTest
 	public void afterTest() {
 		driver.quit();
+	}
+
+	protected void goToMainPage() {
+		driver.get(pathinfoUrl);
+	}
+	
+	protected void login(String username, String password) {
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("loginForm")).submit();
+	}
+	
+	protected void logout() {
+
+		driver.get(pathinfoUrl + "a/logout");
+	}
+	
+	protected void assertExists(String cssSelector) {
+		try {
+			driver.findElement(By.cssSelector(cssSelector));
+		} catch (NoSuchElementException e) {
+			Assert.fail(String.format("Can't find element with selector '%s'.", cssSelector));
+		}
 	}
 }
---
+++ src/test/java/com/lumanmed/pathinfo/autotest/UserTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.lumanmed.pathinfo.autotest;
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+/**
+ * @author jwang25
+ *
+ */
+public class UserTest extends BaseTest {
+	@Test
+	public void testLogin() {
+		logout();
+
+		WebElement selectLanguage = null;
+		try {
+			selectLanguage = driver.findElement(By
+					.cssSelector("#languageSwitch a"));
+		} catch (NoSuchElementException e) {
+			Assert.fail("Can't find languageSwitch element.");
+		}
+		if ("Select Language".equals(selectLanguage.getText())) {
+			System.out.println("Change locale to zh_CN");
+			driver.get(pathinfoUrl + "change_locale/zh_CN?url=" + pathinfoUrl);
+		}
+		
+		login("thinkgem", "admin");
+		assertExists("#menu");
+	}
+	
+	@Test(dependsOnMethods = {"testLogin"})
+	public void testLogout() {
+		logout();
+		assertExists("#languageSwitch");
+	}
+}
